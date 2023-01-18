5a84203ec47e424e480a8926e3d2c160dbbd460d
Paul Anthony McGowan <xhostcom@aol.com>
2023-01-18 16:48:31 +0000

    update

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,4 +1,5 @@
 class PlaysController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_play, only: %i[ show edit update destroy ]
 
   # GET /plays or /plays.json
