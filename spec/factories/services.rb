11113b3c00eb61fcf1d0d5e2146db9fef39e5474
Paul Anthony McGowan <xhostcom@aol.com>
2023-01-18 09:30:00 +0000

    Initial commit

---
+++ spec/factories/services.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :service do
+    user { nil }
+    provider { "MyString" }
+    uid { "MyString" }
+    access_token { "MyString" }
+    access_token_secret { "MyString" }
+    refresh_token { "MyString" }
+    expires_at { "2023-01-18 09:29:39" }
+    auth { "MyText" }
+  end
+end
